6b3a8afc2442b37d64d53bf32cf5969eb18fec25
PHTRU11 <PHTRU11@ulaval.ca>
2024-04-11 12:05:54 -0400

    du taponnage

---
+++ gmat/GMAT/api/test.py
@@ -0,0 +1,3 @@
+from load_gmat import *
+
+sat = gmat.Construct("Spacecraft","LeoSat")
--- projet/test.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(1, r'C:\\Users\\phili.000\\PhysNum\\gmat\\GMAT\\api')
-from load_gmat import *
-
-sat = gmat.Construct("Spacecraft","Sat")
-gmat.ShowObjects()
-
-
