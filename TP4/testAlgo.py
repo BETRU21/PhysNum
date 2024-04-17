eff5d7b512f5e00f47871e00330dbb1989e8839b
Benjamin <benjamin.trudel.2@ulaval.ca>
2024-04-16 23:52:51 -0400

    Début du notebook TP4

--- TP4/testAlgo.py
+++ TP4/testAlgo.py
@@ -2,16 +2,19 @@ from util import readAngles, readSinogram
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.transform import iradon
+import matplotlib.image as mpimg
 
-x, y, sinogram = readSinogram("data/sinogram-patient.txt")
-y, angles = readAngles("data/angles.txt")
+# x, y, sinogram = readSinogram("data/sinogram-patient.txt")
+# y, angles = readAngles("data/angles.txt")
 
-sinogramExample = np.swapaxes(sinogram, 0,1)
-sinogramExample = np.flip(sinogramExample,0)
-image_reconstruite = iradon(sinogramExample, theta=angles*180/np.pi, circle=True)
+# sinogramExample = np.swapaxes(sinogram, 0,1)
+# sinogramExample = np.flip(sinogramExample,0)
+# image_reconstruite = iradon(sinogramExample, theta=angles*180/np.pi, circle=True)
 
-fig, ax = plt.subplots(nrows=1, ncols=2)
-ax[0].imshow(sinogram, cmap="gray")
-ax[1].imshow(image_reconstruite, cmap="gray")
-plt.show()
+# fig, ax = plt.subplots(nrows=1, ncols=2)
+# ax[0].imshow(sinogram, cmap="gray")
+# ax[1].imshow(image_reconstruite, cmap="gray")
+# plt.show()
 
+img = mpimg.imread('phantom-thorax-096-smooth.png')
+print(img.shape)
