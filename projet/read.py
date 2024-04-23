d05775472ff62cb19611a55193aff06fa595b292
PHTRU11 <PHTRU11@ulaval.ca>
2024-04-22 23:05:05 -0400

    I guess juste simul uranus marche pas aussi

--- projet/read.py
+++ projet/read.py
@@ -1,20 +1,23 @@
 import numpy as np
 import os
-import json
 import matplotlib.pyplot as plt
+from cycler import cycler
 
 import matplotlib as mpl
 
 # Set the default color cycle
-mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=["r", "k", "b", 'orange', 'green', 'violet', 'gold', 'gray']) 
+cmap = plt.get_cmap('jet')
+colors = cmap(np.linspace(0, 1, 12))
+linestyle_cycler = cycler('linestyle',['-','--',':','-.'])
 
+plt.rc('axes', prop_cycle=(cycler('color', colors) +
+                           cycler('linestyle', ['-', '--', ':', '-.']*3)))
 
 path = 'projet\\'
 files = os.listdir(path)
 
 i = 0
-ls = ['-', (0, (5,8)), ':', (0, (1,3))]
-fig = plt.figure()
+fig = plt.figure(figsize=(14,8))
 
 for f in files:
     if f[-4:] == '.txt' and f[:-4] != 'temp':
@@ -22,8 +25,12 @@ for f in files:
         data = np.loadtxt(path+f, delimiter=',')
         time = np.linspace(2, len(data[:,0])*2, len(data[:,0]))
 
-        plt.plot(time, data[:,0], label=f'$\\Delta_x$ {f[:-5]}', linestyle=ls[i])
-        plt.plot(time, data[:,1], label=f'$\\Delta_y$ {f[:-5]}', linestyle=ls[i])
+        plt.plot(time, data[:,0], label=f'$\\Delta_x$ {f[:-5]}' )
+        plt.plot(time, data[:,1], label=f'$\\Delta_y$ {f[:-5]}')
+        try:
+            plt.plot(time, data[:,2], label=f'$\\Delta_z$ {f[:-5]}')
+        except IndexError as e:
+            pass
         plt.xlabel('Temps [jour]')
         plt.ylabel('Delta [km]')
         plt.legend()
